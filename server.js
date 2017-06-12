f6c602ba3f02bbfee2fb2ce86342d666e11a29d7
Meggie Kolasani <meghanakolasani@10-248-118-210.wireless.oregonstate.edu>
2017-06-12 04:26:55 -0700

    TONS OF CHANGES

--- server.js
+++ server.js
@@ -11,13 +11,26 @@ var blogData = require('./blog');
 var app = express();
 var port = process.env.PORT || 3000;
 
-var indexFile = fs.readFileSync('./public/index.html');
-var errorFile = fs.readFileSync('./404.html', 'utf8');
+var errorFile = fs.readFileSync('./views/404Page.handlebars', 'utf8');
+var blogss = fs.readFileSync('./views/partials/blogss.handlebars', 'utf8');
+var blogPage = fs.readFileSync('./views/blogPage.handlebars', 'utf8');
 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/Blog.html', function(req, res){
+
+  var templateArgs = {
+    bloggy: blogData
+  };
+  res.render('blogPage', templateArgs);
+});
+
+app.get('*', function (req, res) {
+  res.status(404).render('404Page');
+});
 
 app.listen(port, function (){
   console.log("Server listening on port ", port);
